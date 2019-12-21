eb3b0a32abe8f0b928be4707a9d742a78d2b1eae
Manuel Limones <mlimones@pensare.mx>
2019-12-21 15:13:32 -0600

    Render one recipe

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
-console.log('Hello worl');
+let recipes = [
+  {
+    name: 'cake',
+    ingredients: [
+      {name: 'milk', quantity: 1, unit: 'cup'},
+      {name: 'flour', quantity: 1, unit: 'cup'},
+      {name: 'eggs', quantity: 2, unit: 'piece'},
+    ],
+    steps: [
+      {description: 'mix all'},
+      {description: 'put in bowl'},
+      {description: 'put in oven'},
+      {description: 'take out from oven'},
+      {description: 'let it rest'},
+    ]
+  }
+];
+
+function renderModel(recipes) {
+  let recipe =  recipes[0];
+  document.getElementById('recipe-name').textContent = recipe.name;
+  for(let ingredient of recipe.ingredients ){
+    let ingredientView = createIngredientView(ingredient);
+    document.getElementById('recipe-ingredients').appendChild(ingredientView);
+  }
+
+  for(let step of recipe.steps ){
+    let stepView = createStepView(step);
+    document.getElementById('recipe-steps').appendChild(stepView);
+  }
+}
+
+function createIngredientView(ingredient){
+  let element = document.createElement('li');
+  element.textContent = `${ingredient.name} ${ingredient.quantity} ${ingredient.unit}`;
+  return element;
+}
+
+function createStepView(step){
+  let element = document.createElement('li');
+  element.textContent = `${step.description}`;
+  return element;
+}
+
+
+renderModel(recipes);
