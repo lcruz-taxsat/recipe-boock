bad731452d8d807d33aeacaf627f543d7a48cec5
Manuel Limones <mlimones@pensare.mx>
2019-12-21 15:27:52 -0600

    Render many recipes

--- js/index.js
+++ js/index.js
@@ -13,22 +13,71 @@ let recipes = [
       {description: 'take out from oven'},
       {description: 'let it rest'},
     ]
+  },
+  {
+    name: 'strawberry cake',
+    ingredients: [
+      {name: 'milk', quantity: 1, unit: 'cup'},
+      {name: 'flour', quantity: 1, unit: 'cup'},
+      {name: 'eggs', quantity: 2, unit: 'piece'},
+      {name: 'strawberries', quantity: 10, unit: 'piece'},
+    ],
+    steps: [
+      {description: 'mix all'},
+      {description: 'put in bowl'},
+      {description: 'put in oven'},
+      {description: 'take out from oven'},
+      {description: 'let it rest'},
+    ]
   }
 ];
 
 function renderModel(recipes) {
-  let recipe =  recipes[0];
-  document.getElementById('recipe-name').textContent = recipe.name;
+  let recipesView = document.getElementById('recipes-container');
+
+  for(let recipe of recipes){
+    let recipeView = creteRecipeView(recipe);
+    recipesView.appendChild(recipeView);
+  }
+}
+
+function creteRecipeView(recipe) {
+  let recipeView = document.createElement('div');
+  let recipeNameView = document.createElement('h2');
+  let recipeIngredientsView = document.createElement('ul');
+  let recipeStepsView = document.createElement('ul');
+
+  recipeNameView.textContent = recipe.name;
+
   for(let ingredient of recipe.ingredients ){
     let ingredientView = createIngredientView(ingredient);
-    document.getElementById('recipe-ingredients').appendChild(ingredientView);
+    recipeIngredientsView.appendChild(ingredientView);
   }
 
   for(let step of recipe.steps ){
     let stepView = createStepView(step);
-    document.getElementById('recipe-steps').appendChild(stepView);
+    recipeStepsView.appendChild(stepView);
   }
+
+
+  recipeView.appendChild(recipeNameView);
+
+  let ingredientsTitle = document.createElement('h2');
+  ingredientsTitle.textContent = 'Ingredients:';
+
+  recipeView.appendChild(ingredientsTitle);
+  recipeView.appendChild(recipeIngredientsView);
+
+  let stepsTitle = document.createElement('h2');
+  stepsTitle.textContent = 'Steps:';
+
+  recipeView.appendChild(stepsTitle);
+  recipeView.appendChild(recipeStepsView);
+
+  return recipeView;
 }
+
+
 
 function createIngredientView(ingredient){
   let element = document.createElement('li');
